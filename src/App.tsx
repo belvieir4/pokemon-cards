8be1df6d28b21feac5a024900cc11ba97debfcf5
Isabel Vieira <work.isabelvieira@gmail.com>
2023-12-18 23:03:09 -0700

    feat: initial page markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Card from 'components/Card';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="flex min-h-screen flex-col items-center">
+      <h1 className="text-5xl font-bold text-red-700">Pokemon Cards</h1>
+      <p className="mt-4">
+        A React.js + TypeScript project using Tailwind and consuming the
+        PokéAPI.
       </p>
-    </>
-  )
+      <div className="mt-10 flex w-3/4 flex-col">
+        <div className="flex">
+          <input
+            className="min-w-full flex-1 rounded-l-md border border-slate-300 px-3 py-2 outline-none duration-300 ease-in-out focus:border-slate-800"
+            placeholder="You can search by number or name"
+          />
+          <button className="rounded-r-md bg-red-700 px-5 py-2 text-white duration-300 ease-in-out hover:bg-red-900">
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="mt-12 grid w-full grid-cols-[repeat(auto-fill,_minmax(240px,_1fr))] gap-4">
+        <Card
+          height={20}
+          img="https://place-hold.it/200x200"
+          name="Pikachu"
+          number={40}
+          types={['electric']}
+          weight={30}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
