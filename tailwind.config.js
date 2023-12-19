a3a18f1d66bbb714d0ebb26dbda5a5b3830b5a20
Isabel Vieira <work.isabelvieira@gmail.com>
2023-12-18 22:59:52 -0700

    feat: added tailwind, fontfaces and global styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      noto: ['NotoSans', 'Arial', 'Helvetica', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
